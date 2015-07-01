8b8438bc63d1510996c9c635c8055e975d41d61d
Karolinka <li-nee@gmx.de>
2015-07-01 18:00:07 +0200

    loading local data

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,17 @@
 'use strict';
 
 angular.module('rgraphApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope,$http) {
     $scope.awesomeThings = [
       'me',
       'you',
       'everyone'
     ];
+
+    $http.get('/files/Sample_data.json')
+       .success(function(data){
+          $scope.powerData = data; 
+          console.log(data);               
+        });
+
   });
